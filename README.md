4743c14d57b4a9b528299bfe52c0ca192462f534
Diego <uo293690@uniovi.es>
2025-12-31 12:43:06 +0100

    readme

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # SEMINARIOWEB_UO293690
 
 Diego del Rio Fernandez-> UO293690
+
+Este repositorio ummplementa un sencillo ejemplo de DenoJS para abastecer los contenidos realizados en los seminarios de la asignatura
+
--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,14 @@ const PORT = 8000;
 const handler = (req: Request): Response => {
   const url = new URL(req.url);
 
-  // Endpoint de API (Backend)
+
   if (url.pathname === '/api/hello') {
     return new Response(JSON.stringify({ message: 'UO293690 Diego del Rio' }), {
       headers: { 'content-type': 'application/json' },
     });
   }
 
-  // Servir Frontend (Simulado)
+ 
   if (url.pathname === '/') {
     return new Response('<h1>Frontend en Deno</h1>', {
       headers: { 'content-type': 'text/html' },
